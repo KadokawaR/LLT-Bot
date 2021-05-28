393998c3a8a1ebfb3cc448e3c6da6b7bb986ff79
yhluk <luyinhao@outlook.com>
2021-05-28 21:41:26 +0800

    Update Dice.java

--- src/main/java/lielietea/mirao/plugin/utils/Dice.java
+++ src/main/java/lielietea/mirao/plugin/utils/Dice.java
@@ -1,18 +1,27 @@
 package lielietea.mirao.plugin.utils;
 
+import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.event.events.MessageEvent;
+import net.mamoe.mirai.message.data.MessageChainBuilder;
+
 import java.util.Random;
 
 public class Dice {
-    public static int roll(int bound){
-        return new Random(System.nanoTime()).nextInt(bound)+1;
-    }
+    public static int getNum(int bound){ return new Random(System.nanoTime()).nextInt(bound)+1; }
 
     public static String getResultString(int bound){
-        return String.valueOf(roll(bound));
+        return String.valueOf(getNum(bound));
     }
 
     public static boolean isDiceCommand(int bound, String input){
         return new StringBuilder("/dice").append(bound).toString().equals(input);
+    }
+
+    public static void roll(GroupMessageEvent event, int bound){
+        String message = event.getMessage().contentToString();
+        if(isDiceCommand(bound,message)){
+            event.getSubject().sendMessage(getResultString(bound));
+        }
     }
 
 }
