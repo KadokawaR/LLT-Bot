2b68059257d5aefae9978bbeccc9adcab4390922
白门守望者 <marblegatekeeper@outlook.com>
2021-06-02 17:18:32 +0800

    Update BombCardSessionManager.java

--- src/main/java/lielietea/mirai/plugin/bombcardgame/BombCardSessionManager.java
+++ src/main/java/lielietea/mirai/plugin/bombcardgame/BombCardSessionManager.java
@@ -1,16 +1,37 @@
 package lielietea.mirai.plugin.bombcardgame;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 //还远没写完！
 public class BombCardSessionManager {
+    static int MAXIMUM_CARDS_DRAWN = 10;
     Boolean isActive;
     int cardDrawn;
     Map<CardColorType,CardHolder> cardStacks; //用来存取已抽牌信息
     Map<SpecialNoticeType, List<Long>> noticeData;//用来存储哪些玩家已经被特殊提示过
+    static BombCardSessionManager INSTANCE = new BombCardSessionManager();
 
-    public int getDrawnCardSumByColor(CardColorType color,long qqID){
+    BombCardSessionManager(){
+        isActive=false;
+        cardDrawn=0;
+        cardStacks = new HashMap<>();
+        noticeData = new HashMap<>();
+    }
+
+    public BombCardSessionManager getInstance(){
+        return INSTANCE;
+    }
+    
+    void clearSession(){
+        isActive=false;
+        cardDrawn=0;
+        cardStacks = new HashMap<>();
+        noticeData = new HashMap<>();
+    }
+
+    int getDrawnCardSumByColor(CardColorType color,long qqID){
         if(cardStacks.containsKey(color)){
             return cardStacks.get(color).getDrawnCardSum(qqID);
         } else {
@@ -18,7 +39,7 @@ public class BombCardSessionManager {
         }
     }
 
-    public boolean isDrawnCardInAllColor(long qqID){
+    boolean isDrawnCardInAllColor(long qqID){
         int w,b,r;
         w = getDrawnCardSumByColor(CardColorType.WHITE,qqID);
         b = getDrawnCardSumByColor(CardColorType.BLACK,qqID);
