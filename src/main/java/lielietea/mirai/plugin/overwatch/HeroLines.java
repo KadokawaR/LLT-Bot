380f6d3ce39c9bdde310881b2499cf7328a5fd94
yhluk <luyinhao@outlook.com>
2021-06-05 19:27:10 +0800

    Herolines Updates

--- src/main/java/lielietea/mirai/plugin/overwatch/HeroLines.java
+++ src/main/java/lielietea/mirai/plugin/overwatch/HeroLines.java
@@ -1,5 +1,18 @@
 package lielietea.mirai.plugin.overwatch;
 
-public class HeroLines {
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
+
+// 在研究Gson的序列化和反序列化
+public class HeroLines {
+    public class Hero{
+        public String name;
+        public ArrayList<String> lines;
+    }
+    public void fromJson(){
+        Gson object = new Gson();
+        Hero hero = object.fromJson("overwatch/herolines.json", Hero.class);
+    }
 }
