b468a4e2c21bf58111ad26523028e36435f9dc0f
白门守望者 <marblegatekeeper@outlook.com>
2021-12-16 13:12:05 +0800

    Refactor
    [remove] Remove unused Interface and deprecated comments.
    [refactor] Make Type Adapter a nested class.

--- src/main/java/lielietea/mirai/plugin/core/messagehandler/game/GameHandler.java
+++
@@ -1,8 +0,0 @@
-package lielietea.mirai.plugin.core.messagehandler.game;
-
-import lielietea.mirai.plugin.core.messagehandler.MessageHandler;
-import net.mamoe.mirai.event.events.GroupMessageEvent;
-
-public interface GameHandler extends MessageHandler<GroupMessageEvent> {
-    //TODO: 看了眼Jetpack 明天再看怎么改成一套体系之下的东西吧
-}
